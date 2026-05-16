cb3a02cf3fb7137c37a7886ddff68ce170b02df4
Ghostik <karenheister5@gmail.com>
2026-05-16 10:40:56 +0300

    refactor: rename auth page to sigh up page and delete button to home

--- apps/mobile/lib/core/routing/router.dart
+++ apps/mobile/lib/core/routing/router.dart
@@ -1,6 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:mobile/core/routing/rout_names.dart';
-import 'package:mobile/features/auth/presentation/auth_page.dart';
+import 'package:mobile/features/auth/presentation/pages/sigh_up_page.dart';
 import 'package:mobile/features/home/presentation/home_page.dart';
 import 'package:mobile/features/splash/presentation/pages/splash_page.dart';
 
@@ -8,7 +8,7 @@ final appRouter = GoRouter(
   initialLocation: Routes.splash,
   routes: [
     GoRoute(path: Routes.splash, builder: (context, state) => const SplashPage()),
-    GoRoute(path: Routes.login, builder: (context, state) => const AuthPage()),
+    GoRoute(path: Routes.login, builder: (context, state) => const SighUpPage()),
     GoRoute(path: Routes.home, builder: (context, state) => const HomePage()),
   ],
 );
